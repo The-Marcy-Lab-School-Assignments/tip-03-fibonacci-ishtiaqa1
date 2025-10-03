78d3577e87bf3792b72dfd44cfce628684c7b99d
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-10-03 10:30:15 -0400

    fixed no arrays

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
-    let arr = [0,1];
+    let a = 0;
+    let b = 1;
     console.log(0);
     console.log(1);
     for (let i = 2; i < n; i++) {
-        arr.push(Math.abs(arr[i-1] + arr[i-2]));
-        console.log(arr[i]);
+        let c = a+b;
+        console.log(c);
+        a = b;
+        b = c;
     }
 }
 
