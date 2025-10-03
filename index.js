e5ce656d64e96774964166160dca169d0d41a6fa
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-10-03 09:43:13 -0400

    Finished Fibonnaci

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let arr = [0,1];
+    console.log(0);
+    console.log(1);
+    for (let i = 2; i < n; i++) {
+        arr.push(Math.abs(arr[i-1] + arr[i-2]));
+        console.log(arr[i]);
+    }
 }
 
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
